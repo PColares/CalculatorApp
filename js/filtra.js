67aeb8ce837e04a69498be906de89200da834cbd
Pedro Siqueira Colares <pedro.colares_BOLS@compasso.com.br>
2021-10-23 16:52:14 -0300

    Add um monte de coisa

--- js/filtra.js
+++ js/filtra.js
@@ -2,24 +2,24 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
     console.log(this.value);
-    var pacientes = document.querySelectorAll(".paciente");
+    var produtos = document.querySelectorAll(".produto");
 
     if(this.value.length>0){
-        for(var i=0; i<pacientes.length; i++){
-            var paciente=pacientes[i];
-            var tdNome = paciente.querySelector(".info-nome");
+        for(var i=0; i<produtos.length; i++){
+            var produto=produtos[i];
+            var tdNome = produto.querySelector(".info-nome");
             var nome = tdNome.textContent;
             var expressao = new RegExp(this.value,"i");
             if(!expressao.test(nome)){
-                paciente.classList.add("invisivel");
+                produto.classList.add("invisivel");
             }else{
-                paciente.classList.remove("invisivel")
+                produto.classList.remove("invisivel")
             }
         }
     }else{
-        for (var i = 0; i < pacientes.length; i++) {
-            paciente = pacientes[i];
-            paciente.classList.remove("invisivel")
+        for (var i = 0; i < produtos.length; i++) {
+            produto = produtos[i];
+            produto.classList.remove("invisivel")
         }
     }
 });
--- js/form.js
+++ js/form.js
@@ -1,65 +1,60 @@
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-    event.preventDefault();
+var botaoAdicionar = document.querySelector("#adicionar-produto");
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    var form = document.querySelector("#formAd");
+  var form = document.querySelector("#formAd");
 
-    var paciente = obtemInfoDoForm(form);
-    console.log(paciente);
+  var produto = obtemInfoDoForm(form);
 
-    
+  adicionaProdutoNaTabela(produto);
 
-    if(!validaPaciente(paciente)){
-        alert("paciente invalido");
-        return;
-    }
-
-    adicionaPacienteNaTabela(paciente);
-
-    form.reset();
+  form.reset();
 });
 
-function adicionaPacienteNaTabela(paciente){
-    var pacienteTr = montaTr(paciente);
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
-
+function adicionaProdutoNaTabela(produto) {
+  var produtoTr = montaTr(produto);
+  var tabela = document.querySelector("#tabela-produtos");
+  tabela.appendChild(produtoTr);
 }
-function obtemInfoDoForm(form){
-    var paciente={
-        nome: form.nome.value,
-        peso: form.peso.value,
-        altura: form.altura.value,
-        gordura: form.gordura.value,
-        imc: calculaImc(form.peso.value, form.altura.value)
-    }
-    return paciente;
-}
-
-function montaTr(paciente){
-    var pacienteTr = document.createElement("tr");
-    pacienteTr.classList.add("paciente");
-
-    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
-    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
-    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
-    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
-    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
-
-    return pacienteTr
-}
-function montaTd(dado,classe){
-    var td = document.createElement("td");
-    td.textContent = dado;
-    td.classList.add(classe);
-
-    return td;
+function obtemInfoDoForm(form) {
+  var produto = {
+    nome: form.nome.value,
+    valorA: form.valorA.value,
+    valorB: form.valorB.value,
+    multi1: form.multi1.value,
+    multi2: form.multi2.value,
+    cxa: form.cxa.value,
+    qtd: form.qtd.value,
+    imc: calculaImc(
+      form.valorA.value.replace(",", "."),
+      form.valorB.value.replace(",", "."),
+      form.multi1.value.replace(",", "."),
+      form.multi2.value.replace(",", "."),
+      form.cxa.value.replace(",", "."),
+      form.qtd.value.replace(",", ".")
+    ),
+  };
+  return produto;
 }
 
-function validaPaciente(paciente){
-    if(validaPeso(paciente.peso)){
-        return true;
-    }else{
-        return false;
-    }
+function montaTr(produto) {
+  var produtoTr = document.createElement("tr");
+  produtoTr.classList.add("produto");
+
+  produtoTr.appendChild(montaTd(produto.nome, "info-nome"));
+  produtoTr.appendChild(montaTd(produto.valorA, "valorA"));
+  produtoTr.appendChild(montaTd(produto.valorB, "valorB"));
+  produtoTr.appendChild(montaTd(produto.multi1, "multi1"));
+  produtoTr.appendChild(montaTd(produto.multi2, "multi2"));
+  produtoTr.appendChild(montaTd(produto.cxa, "cxa"));
+  produtoTr.appendChild(montaTd(produto.qtd, "qtd"));
+  produtoTr.appendChild(montaTd(produto.imc, "info-imc"));
+  return produtoTr;
+}
+function montaTd(dado, classe) {
+  var td = document.createElement("td");
+  td.textContent = dado;
+  td.classList.add(classe);
+
+  return td;
 }
